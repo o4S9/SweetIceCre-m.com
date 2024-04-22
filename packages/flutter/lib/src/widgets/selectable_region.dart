f1d9cf0662390a0ad7020a771eff6d585a8e68ae
Renzo Olivares <rmolivares@renzo-olivares.dev>
2024-04-21 17:25:42 -0700

    _SelectableTextContainerDelegate should extend from MultiStaticSelectableSelectionContainer

--- packages/flutter/lib/src/widgets/selectable_region.dart
+++ packages/flutter/lib/src/widgets/selectable_region.dart
@@ -1611,6 +1611,22 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
   Offset? _lastStartEdgeUpdateGlobalPosition;
   Offset? _lastEndEdgeUpdateGlobalPosition;
 
+  void updateLastStartEdgeUpdateGlobalPosition(Offset globalPosition) {
+    _lastStartEdgeUpdateGlobalPosition = globalPosition;
+  }
+
+  void updateLastEndEdgeUpdateGlobalPosition(Offset globalPosition) {
+    _lastEndEdgeUpdateGlobalPosition = globalPosition;
+  }
+
+  bool markReceivedStartEvent(Selectable selectable) {
+    return _hasReceivedStartEvent.add(selectable);
+  }
+
+  bool markReceivedEndEvent(Selectable selectable) {
+    return _hasReceivedEndEvent.add(selectable);
+  }
+
   @override
   void remove(Selectable selectable) {
     _hasReceivedStartEvent.remove(selectable);
@@ -1618,7 +1634,7 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
     super.remove(selectable);
   }
 
-  void _updateLastEdgeEventsFromGeometries() {
+  void updateLastEdgeEventsFromGeometries() {
     if (currentSelectionStartIndex != -1 && selectables[currentSelectionStartIndex].value.hasSelection) {
       final Selectable start = selectables[currentSelectionStartIndex];
       final Offset localStartEdge = start.value.startSelectionPoint!.localPosition +
@@ -1633,6 +1649,14 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
     }
   }
 
+  void updateLastEdgeEventsFromSelectionEdgeUpdateEvent(SelectionEdgeUpdateEvent event) {
+    if (event.type == SelectionEventType.endEdgeUpdate) {
+      _lastEndEdgeUpdateGlobalPosition = event.globalPosition;
+    } else {
+      _lastStartEdgeUpdateGlobalPosition = event.globalPosition;
+    }
+  }
+
   @override
   SelectionResult handleSelectAll(SelectAllSelectionEvent event) {
     final SelectionResult result = super.handleSelectAll(event);
@@ -1641,7 +1665,7 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
       _hasReceivedEndEvent.add(selectable);
     }
     // Synthesize last update event so the edge updates continue to work.
-    _updateLastEdgeEventsFromGeometries();
+    updateLastEdgeEventsFromGeometries();
     return result;
   }
 
@@ -1656,7 +1680,7 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
     if (currentSelectionEndIndex != -1) {
       _hasReceivedEndEvent.add(selectables[currentSelectionEndIndex]);
     }
-    _updateLastEdgeEventsFromGeometries();
+    updateLastEdgeEventsFromGeometries();
     return result;
   }
 
@@ -1671,7 +1695,7 @@ class MultiStaticSelectableSelectionContainerDelegate extends MultiSelectableSel
     if (currentSelectionEndIndex != -1) {
       _hasReceivedEndEvent.add(selectables[currentSelectionEndIndex]);
     }
-    _updateLastEdgeEventsFromGeometries();
+    updateLastEdgeEventsFromGeometries();
     return result;
   }
 
