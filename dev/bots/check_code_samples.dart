e253c77a245afdd381788e0017cc7094855f467c
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-05-02 21:01:11 +0200

    Add tests for logical_key_set.0.dart API example.

--- dev/bots/check_code_samples.dart
+++ dev/bots/check_code_samples.dart
@@ -431,7 +431,6 @@ final Set<String> _knownMissingTests = <String>{
   'examples/api/test/widgets/image/image.error_builder.0_test.dart',
   'examples/api/test/widgets/image/image.frame_builder.0_test.dart',
   'examples/api/test/widgets/image/image.loading_builder.0_test.dart',
-  'examples/api/test/widgets/shortcuts/logical_key_set.0_test.dart',
   'examples/api/test/widgets/shortcuts/shortcuts.0_test.dart',
   'examples/api/test/widgets/shortcuts/shortcuts.1_test.dart',
   'examples/api/test/widgets/shortcuts/callback_shortcuts.0_test.dart',
