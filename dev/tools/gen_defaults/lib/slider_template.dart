b1710e726704896253f709d94914a43c43db8349
Taha Tesser <tessertaha@gmail.com>
2024-05-03 16:23:17 +0300

    Introduce new `Slider` shapes

--- dev/tools/gen_defaults/lib/slider_template.dart
+++ dev/tools/gen_defaults/lib/slider_template.dart
@@ -14,17 +14,25 @@ class SliderTemplate extends TokenTemplate {
   @override
   String generate() => '''
 class _${blockName}DefaultsM3 extends SliderThemeData {
-  _${blockName}DefaultsM3(this.context)
-    : super(trackHeight: ${getToken('$tokenGroup.active.track.height')});
+  _${blockName}DefaultsM3({ required this.context, required SliderThemeData sliderTheme })
+    : _sliderTheme = sliderTheme;
 
   final BuildContext context;
+  late final ThemeData theme = Theme.of(context);
   late final ColorScheme _colors = Theme.of(context).colorScheme;
+  final SliderThemeData _sliderTheme;
 
   @override
   Color? get activeTrackColor => ${componentColor('$tokenGroup.active.track')};
 
   @override
-  Color? get inactiveTrackColor => ${componentColor('$tokenGroup.inactive.track')};
+  Color? get inactiveTrackColor {
+    if (_sliderTheme.trackShape is ExpressiveRoundedRectSliderTrackShape) {
+      // TODO(tahatesser): Update this hard-coded value to use the latest tokens.
+      return _colors.secondaryContainer;
+    }
+    return ${componentColor('$tokenGroup.inactive.track')};
+  }
 
   @override
   Color? get secondaryActiveTrackColor => _colors.primary.withOpacity(0.54);
@@ -57,27 +65,68 @@ class _${blockName}DefaultsM3 extends SliderThemeData {
   Color? get disabledThumbColor => Color.alphaBlend(${componentColor('$tokenGroup.disabled.handle')}, _colors.surface);
 
   @override
-  Color? get overlayColor => MaterialStateColor.resolveWith((Set<MaterialState> states) {
-    if (states.contains(MaterialState.dragged)) {
-      return ${componentColor('$tokenGroup.pressed.state-layer')};
+  Color? get overlayColor {
+    if (_sliderTheme.thumbShape is BarSliderThumbShape && theme.brightness != Brightness.dark) {
+      return Colors.transparent;
     }
-    if (states.contains(MaterialState.hovered)) {
-      return ${componentColor('$tokenGroup.hover.state-layer')};
-    }
-    if (states.contains(MaterialState.focused)) {
-      return ${componentColor('$tokenGroup.focus.state-layer')};
-    }
+    return MaterialStateColor.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.dragged)) {
+        return ${componentColor('$tokenGroup.pressed.state-layer')};
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return ${componentColor('$tokenGroup.hover.state-layer')};
+      }
+      if (states.contains(MaterialState.focused)) {
+        return ${componentColor('$tokenGroup.focus.state-layer')};
+      }
 
-    return Colors.transparent;
-  });
+      return Colors.transparent;
+    });
+  }
 
   @override
   TextStyle? get valueIndicatorTextStyle => ${textStyle('$tokenGroup.label.label-text')}!.copyWith(
-    color: ${componentColor('$tokenGroup.label.label-text')},
+    color:  _sliderTheme.valueIndicatorShape is RoundedRectSliderValueIndicatorShape
+      ? _colors.onInverseSurface
+      : ${componentColor('$tokenGroup.label.label-text')},
   );
 
   @override
   SliderComponentShape? get valueIndicatorShape => const DropSliderValueIndicatorShape();
+
+  @override
+  Color? get valueIndicatorColor {
+    if (_sliderTheme.valueIndicatorShape is RoundedRectSliderValueIndicatorShape) {
+      return _colors.inverseSurface;
+    }
+    return ${componentColor('$tokenGroup.label.container')};
+  }
+
+  @override
+  SliderTrackShape? get trackShape => const RoundedRectSliderTrackShape();
+
+  @override
+  double? get trackHeight => _sliderTheme.trackShape is ExpressiveRoundedRectSliderTrackShape
+    // TODO(tahatesser): Update this hard-coded value to use the latest tokens.
+    ? 16.0
+    : ${getToken("$tokenGroup.active.track.height")};
+
+  @override
+  SliderTickMarkShape? get tickMarkShape => const RoundSliderTickMarkShape();
+
+  @override
+  SliderComponentShape? get overlayShape => const RoundSliderOverlayShape();
+
+  @override
+  SliderComponentShape? get thumbShape => const RoundSliderThumbShape();
+
+  // TODO(tahatesser): Update this hard-coded value to use the latest tokens.
+  @override
+  Size? get barThumbSize => const Size(4.0, 44.0);
+
+  // TODO(tahatesser): Update this hard-coded value to use the latest tokens.
+  @override
+  double? get trackGapSize => 6.0;
 }
 ''';
 
